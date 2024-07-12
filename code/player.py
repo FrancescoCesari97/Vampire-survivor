99026282749cd9dd590fb2bdf2e3c2260e070660
FrancescoCesari97 <freedus97@gmail.com>
2024-07-12 12:46:38 +0200

    added normalize to fix the speed of diagonal movements

--- code/player.py
+++ code/player.py
@@ -15,6 +15,7 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
         self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction #*->to make the diagolan movement the same speed
 
     def move(self, dt):
         self.rect.center += self.direction * self.speed * dt
