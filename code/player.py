d8d3e081f3d57c4d3aa72b7fd85583cf0a10376b
FrancescoCesari97 <freedus97@gmail.com>
2024-07-12 12:42:30 +0200

    added basic movement with keys press

--- code/player.py
+++ code/player.py
@@ -6,3 +6,19 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
+
+        # * movement
+        self.direction = pygame.Vector2()
+        self.speed = 400
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+
+    def move(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+    
+    def update(self, dt):
+        self.input()
+        self.move(dt)
