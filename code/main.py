05105e873e2959c5ff00bf42a574e025707e1d27
FrancescoCesari97 <freedus97@gmail.com>
2024-07-16 11:31:41 +0200

    created camera that follows the player movement

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import *
 
@@ -15,7 +16,7 @@ class Game:
         self.running = True
 
         # * groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
@@ -57,7 +58,7 @@ class Game:
 
             # * draw
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
