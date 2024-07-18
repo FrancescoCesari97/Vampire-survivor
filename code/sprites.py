efb5a327111a69b8e57645987d3c1bba3661c24a
FrancescoCesari97 <freedus97@gmail.com>
2024-07-18 11:00:53 +0200

    added collision to the enemy

--- code/sprites.py
+++ code/sprites.py
@@ -70,6 +70,7 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups, player, collision_sprites):
         super().__init__(groups)
         self.player = player
+       
 
         # * image
         self.frames, self.frame_index = frames, 0
@@ -82,6 +83,7 @@ class Enemy(pygame.sprite.Sprite):
         self.collision_sprites = collision_sprites
         self.direction = pygame.Vector2()
         self.speed = 350
+        self.hitbox_rect = self.rect.inflate(-55, -90)
 
     def animate(self, dt):
         self.frame_index += self.animation_speed * dt
@@ -94,7 +96,21 @@ class Enemy(pygame.sprite.Sprite):
         self.direction = (player_pos - enemy_pos).normalize()
 
         # * update the rect position + collision logic
-        self.rect.center += self.direction * self.speed * dt
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
+    
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
+                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
     
     def update(self, dt):
         self.move(dt)
